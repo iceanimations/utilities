adad7ee2cb64423ca741476c0df3f4b7f5403d6f
Talha Ahmed <talha.ahmed@gmail.com>
2014-11-20 14:57:29 +0500

    PyQt now available for maya2011-15

--- uiContainer.py
+++ uiContainer.py
@@ -5,7 +5,10 @@ import sys
 import logging
 
 version = int(re.search('\\d{4}', pc.about(v=True)).group())
-if version == 2014:
+if version in range(2011, 2016):
+    site.addsitedir(r"R:\Python_Scripts\maya"+str(version)+r"\PyQt")
+    from PyQt4 import uic
+else:
     import PySide as PyQt4
     sys.modules["PyQt4"] = PyQt4
     import uiLoader
@@ -14,9 +17,6 @@ if version == 2014:
     import shiboken as sip
     sip.wrapinstance = sip.wrapInstance
     sys.modules['sip'] = sip
-else:
-    site.addsitedir(r"R:\Python_Scripts\maya"+str(version)+r"\PyQt")
-    from PyQt4 import uic
 
 for uic_subm in ['.properties', '.uiparser']:
     logger = logging.getLogger(uic.__name__ + uic_subm)
