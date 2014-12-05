43940b20daa5379a30bb4fd5ef7f33aab2cf62fa
= <=>
2014-12-05 18:19:42 +0500

    nukeMenuCommands.py added

--- createNukeMenu.py
+++ createNukeMenu.py
@@ -1,17 +1,10 @@
-from site import addsitedir as asd
-asd(r'R:\Python_Scripts\Nuke')
-asd('R:/Python_Scripts/maya2014/PyQt')
 import nuke
-
-def replaceReadPaths():
-    import replaceReadPath as rrp
-    reload(rrp)
-    rrp.Window().show()
+from nukeMenuCommands import *
 
 # add new script to the following dict
 # key => name of the script that will appear in the nuke under ICE Scripts menu
 # value => name of the function to be called when user clicks that menu item
-# finally create that function somewhere above the dict
+# finally create that function in the nukeMenuCommands.py
 
 nukeMenu = {
             'Replace Read Paths': replaceReadPaths
---
+++ nukeMenuCommands.py
@@ -0,0 +1,4 @@
+def replaceReadPaths():
+    import replaceReadPath as rrp
+    reload(rrp)
+    rrp.Window().show()
