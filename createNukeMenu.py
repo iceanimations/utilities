b4a40aa93fe67a7496c0ed4406741403d680928e
= <=>
2014-12-05 17:24:48 +0500

    functions moved up

--- createNukeMenu.py
+++ createNukeMenu.py
@@ -3,20 +3,19 @@ asd(r'R:\Python_Scripts\Nuke')
 asd('R:/Python_Scripts/maya2014/PyQt')
 import nuke
 
-# add new script to the following dict
-# key => name of the script that will appear in the nuke under ICE Scripts menu
-# value => name of the function to be called when user clicks that menu item
-# finally create that function in the bottom of this module
-
-nukeMenu = {
-            'Replace Read Paths': replaceReadPaths
-            }
-
-
 def replaceReadPaths():
     import replaceReadPath as rrp
     reload(rrp)
     rrp.Window().show()
+
+# add new script to the following dict
+# key => name of the script that will appear in the nuke under ICE Scripts menu
+# value => name of the function to be called when user clicks that menu item
+# finally create that function somewhere above the dict
+
+nukeMenu = {
+            'Replace Read Paths': replaceReadPaths
+            }
 
 def create():
     for name, func in nukeMenu.items():
