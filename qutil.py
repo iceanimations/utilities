ed331bbde8e6d52b24ae6a65b8d9b74a3c418fbf
Qurban <qurban_ali36@yahoo.com>
2015-01-06 11:37:04 +0500

    getAttrRecursiveGroup function added in qutil.py

--- qutil.py
+++ qutil.py
@@ -1,7 +1,12 @@
 import os
+import operator
 import os.path as osp
 
+
 def splitPath(path):
+    '''splits a file or folder path and returns as a list
+    'D:/path/to/folder/or/file' -> ['D:', 'path', 'to', 'folder', 'or', 'file']
+    '''
     components = [] 
     while True:
         (path,tail) = os.path.split(path)
@@ -11,9 +16,22 @@ def splitPath(path):
         components.append(tail)
 
 def basename3(path):
+    '''returns last 3 entries in a file or folder path as a string'''
     return osp.join(*splitPath(path)[-3:])
 
 def mkdir(path, dirs):
+    '''makes directories or folders recursively in a given path'''
     for d in splitPath(dirs):
         path = osp.join(path, d)
         os.mkdir(path)
+        
+def getAttrRecursiveGroup(node, attribute):
+    '''returns the specified attribute of a node traversing to the last parent'''
+    attr = (0, 0, 0)
+    for _ in range(200):
+        attr = tuple(operator.add(attr, pc.PyNode(str(node)+ '.'+ attribute).get()))
+        try:
+            node= node.firstParent()
+        except pc.MayaNodeError:
+            break
+    return attr
