28b95537a0ce1ae2160b09e57988d7d3aaeb83e6
Talha Ahmed <talha.ahmed@gmail.com>
2017-07-17 17:38:36 +0500

    Error Nodes UI

--- createNukeMenu.py
+++ createNukeMenu.py
@@ -1,11 +1,14 @@
 from site import addsitedir as asd
 asd('R:/Python_Scripts/plugins')
+
 import sip
 sip.setapi('QString', 2)
+
 import nuke
 import nukeMenuCommands
 reload(nukeMenuCommands)
 from nukeMenuCommands import *
+
 import startup
 reload(startup)
 
@@ -25,15 +28,17 @@ nukeMenu = {
             'Add Write Node': [addWriteNodes, 'Ctrl+Alt+W'],
             'Reread Frame Range': [reread, 'Ctrl+Alt+F'],
             'Replace Cameras In Backdrops': [replaceBackdropCameras,
-                'Ctrl+Alt+C'],
+                                             'Ctrl+Alt+C'],
             'Render Threads': [renderThreads, ''],
             'Select Error Nodes': [selectErrorNodes, '']
-            }
+}
+
 menuName = 'ICE Scripts/'
+
+
 def create():
     nuke.menu('Nuke').removeItem(menuName[0:-1])
     for name, func in nukeMenu.items():
-        nuke.menu('Nuke').addCommand(menuName+ name, func[0], func[1])
+        nuke.menu('Nuke').addCommand(menuName+name, func[0], func[1])
     nuke.menu('Nuke').menu(menuName[0:-1]).addSeparator()
     nuke.menu('Nuke').addCommand(menuName+'Rebuild this Menu', rebuildMenu)
-    
--- nukeMenuCommands.py
+++ nukeMenuCommands.py
@@ -1,57 +1,70 @@
 import createNukeMenu
-reload(createNukeMenu)
 import nuke
+
+reload(createNukeMenu)
+
 
 def replaceReadPaths():
     import replaceReadPath as rrp
     reload(rrp)
     rrp.Window().show()
-    
+
+
 def fromRedToDefault():
     import redToDefault as rd
     reload(rd)
     rd.change()
-    
+
+
 def renderWrites():
     import renderWrite
     reload(renderWrite)
     renderWrite.render()
-    
+
+
 def rebuildMenu():
     reload(createNukeMenu)
     createNukeMenu.create()
-    
+
+
 def setNearestFrame():
     for node in nuke.selectedNodes('Read'):
         node.knob('on_error').setValue(3)
+
 
 def saveIncrement():
     import autoSave
     reload(autoSave)
     autoSave.Window().show()
-    
+
+
 def addWriteNodes():
     import addWrite
     reload(addWrite)
     addWrite.addWrite()
+
 
 def reread():
     import rereadFrameRange as rr
     reload(rr)
     rr.read()
 
+
 def replaceBackdropCameras():
     import replaceCamera
     reload(replaceCamera)
     print replaceCamera.replaceBackdropCameras()
+
 
 def renderThreads():
     from renderthreads import renderthreads
     reload(renderthreads)
     renderthreads.run()
 
+
 def selectErrorNodes():
-    from errorNodes import main, error_nodes
+    from errorNodes import main, error_nodes, error_nodes_ui
     reload(error_nodes)
+    reload(error_nodes_ui)
     reload(main)
-    main.main()
+    main.main_gui()
